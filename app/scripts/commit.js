f34d0755d2ffce59b31a0751ec7ebf795c82c290
stafford <stafford.williams@gmail.com>
2018-03-10 22:50:50 +1100

    fix pjax loading

--- app/scripts/commit.js
+++ app/scripts/commit.js
@@ -1,15 +1,23 @@
 'use strict';
 
-var commit = $('.commit-meta span:contains(parent) a');
-var tooltip = 'rel="nofollow" aria-label="View the whole file at version @p"'.replace('@p', commit.text());
-var button = '<a href="@link" class="btn btn-sm tooltipped tooltipped-n" @attr >Left</a>'.replace('@attr', tooltip);
+function addLeftRight() {
+	var commit = $('.commit-meta span:contains(parent) a');
+	var tooltip = 'rel="nofollow" aria-label="View the whole file at version @p"'.replace('@p', commit.text());
+	var button = '<a href="@link" class="btn btn-sm tooltipped tooltipped-n" @attr >Left</a>'.replace('@attr', tooltip);
 
-$('.file-info a:contains(→)').each(function () {
-	var oldFile = $(this).attr('title').split(' → ')[0];
-	var link = commit.attr('href').replace('commit', 'blob') + '/' + oldFile;
-	var parent = $(this).parent().parent();
+	$('.file-info a:contains(→)').each(function () {
+		var oldFile = $(this).attr('title').split(' → ')[0];
+		var link = commit.attr('href').replace('commit', 'blob') + '/' + oldFile;
+		var parent = $(this).parent().parent();
 
-	parent.find('.file-actions a').text('Right');
-	$(button.replace('@link', link)).insertAfter(parent.find('.show-file-notes'));
+		parent.find('.file-actions a').text('Right');
+		$(button.replace('@link', link)).insertAfter(parent.find('.show-file-notes'));
+	});
+}
+
+document.addEventListener('pjax:end', function () {
+	addLeftRight();
 });
+
+addLeftRight();
 //# sourceMappingURL=commit.js.map
