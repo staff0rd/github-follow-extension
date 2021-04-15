58962af08e686b05a709e613db8bad462adbf4f1
Jeff Stieler <jeff.m.stieler@gmail.com>
2021-04-15 09:13:05 -0500

    Fix file rename follow button injection and styling.

--- app/scripts/history.js
+++ app/scripts/history.js
@@ -53,9 +53,9 @@ function follow(sha, fileName, ownerRepo) {
 		// var style = '<style>.icon-follow { background-image: url(:iconUrl); background-position: center center; width:16px; height:16px; display: inline-block;</style>'
 		// 	.replace(':iconUrl', iconUrl);
 
-		var template = ['<div id="github-follow" class="commit-group-title"><span class="octicon octicon-git-commit"></span>', '<strong>Renamed</strong> to :fileName <a class="btn right" href=":link">Follow</a></div>'].join('').replace(':fileName', fileName).replace(':link', link);
+		var template = ['<div class="TimelineItem TimelineItem--condensed">', '<div class="TimelineItem-badge">', '<svg class="octicon octicon-git-commit" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M10.5 7.75a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm1.43.75a4.002 4.002 0 01-7.86 0H.75a.75.75 0 110-1.5h3.32a4.001 4.001 0 017.86 0h3.32a.75.75 0 110 1.5h-3.32z"></path></svg>', '</div>', '<div class="TimelineItem-body">', '<h2 class="f5 text-normal">', '<strong>Renamed</strong> to :fileName', '<a class="btn right" href=":link">Follow</a>', '</h2>', '</div>', '</div>'].join('').replace(':fileName', fileName).replace(':link', link);
 
-		$('.commits-listing').append(template);
+		$('.js-navigation-container.mt-3').append(template);
 	});
 }
 
